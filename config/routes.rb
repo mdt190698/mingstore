dc912fe2058d55b237f676c2fa1260af6ab80079
mdt190698 <nameno220200@gmail.com>
2025-01-04 14:07:00 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :products
+  resource :session
+  resources :passwords, param: :token
+  get "/products", to: "products#index"
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
   root "products#index"
-
 end
